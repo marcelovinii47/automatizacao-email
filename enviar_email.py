f461b7df15a7a3a3420ded52fa95564590b798c6
Marcelo <marcelo.sousa31@fatec.sp.gov.br>
2024-09-26 18:55:21 -0300

    Automação simples de email

---
+++ enviar_email.py
@@ -0,0 +1,30 @@
+import smtplib 
+from email.mime.multipart import MIMEMultipart #Classe que permite textos e anexos no email 
+from email.mime.text import MIMEText #Classe que cria corpo do email em formato de texto
+
+remetente = 'vinicusm466@gmail.com'
+destinatario = 'vinicusm466@gmail.com'
+assunto = 'Teste de envio de e-mail'
+corpo = 'Olá, este é um e-mail enviado através de Python!'
+
+msg = MIMEMultipart()
+msg['From'] = remetente
+msg['To'] = destinatario
+msg['Subject'] = assunto
+msg.attach(MIMEText(corpo, 'plain'))
+
+# Servidor SMTP
+smtp_server = 'smtp.gmail.com'
+porta = 587
+senha = 'wxqx mesf afln qpaf'
+
+try:
+    servidor = smtplib.SMTP(smtp_server, porta)
+    servidor.starttls()
+    servidor.login(remetente, senha)
+    servidor.send_message(msg)
+    print('E-mail enviado com sucerro!')
+except Exception as e:
+    print(f'Falha ao enviar o e-mail: {e}')
+finally:
+    servidor.quit()
